d8525152d33789cd6ea75e140f8301b9af4ae5b6
vijexa <nuzhdinvitja@gmail.com>
2020-09-26 14:49:42 +0300

    fixed indentation

--- src/main/scala/homework1/Homework1.scala
+++ src/main/scala/homework1/Homework1.scala
@@ -1,7 +1,7 @@
 package homework1
 
 object Homework1 {
-    def lcm(a: Int, b: Int): Int = scala.math.abs(a * b) / gcd(a, b)
+  def lcm(a: Int, b: Int): Int = scala.math.abs(a * b) / gcd(a, b)
 
-    def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
