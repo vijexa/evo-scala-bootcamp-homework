066ab3d3a74a8799a92efa1c17bb90804b0bddd7
vijexa <nuzhdinvitja@gmail.com>
2020-09-26 14:31:07 +0300

    init

---
+++ src/test/scala/homework1/Homework1Spec.scala
@@ -0,0 +1,20 @@
+package homework1
+
+class Homework1Test extends org.scalatest.FunSuite {
+  test("Homework1.lcm") {
+    assert(Homework1.lcm(6, 9) === 18)
+    assert(Homework1.lcm(3, 9) === 9)
+    assert(Homework1.lcm(4, 8) === 8)
+    assert(Homework1.lcm(25, 10) === 50)
+    assert(Homework1.lcm(100, 10) === 100)
+  }
+
+  test("Homework1.gcd") {
+    assert(Homework1.gcd(8, 12) === 4)
+    assert(Homework1.gcd(6, 9) === 3)
+    assert(Homework1.gcd(3, 9) === 3)
+    assert(Homework1.gcd(4, 8) === 4)
+    assert(Homework1.gcd(25, 10) === 5)
+    assert(Homework1.gcd(100, 10) === 10)
+  }
+}
